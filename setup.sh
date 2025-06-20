eed084fff9d7bee84d7ea509d37ac951cd54128a
Yusuke_Kanazawa <c1005073@planet.kanazawa-it.ac.jp>
2025-06-20 09:44:29 +0900

    Updata README.md

--- setup.sh
+++ setup.sh
@@ -1,12 +1,19 @@
 # zipファイル解凍
+echo "Unzipping large_files_docker_ReID3D2025.zip ..."
 unzip $HOME/large_files_docker_ReID3D2025.zip
+echo "unzipped!"
 
 # 移動
-cp -r $HOME/large_files_docker_ReID3D2025/ReID3D $HOME/docker_ReID3D2025
-cp -r $HOME/large_files_docker_ReID3D2025/ros2_bags $HOME/docker_ReID3D2025
+echo -e "\nCopying files ..."
+cp -r $HOME\large_files_docker_ReID3D2025/ReID3D $HOME/docker_ReID3D2025/
+cp -r $HOME\large_files_docker_ReID3D2025/ros2_bags $HOME/docker_ReID3D2025/
+echo "Copied!"
 
 # Docker IMAGE を取り込み
-docker load < $HOME/large_files_docker_ReID3D2025/dockerimage_reid3d.tar.gz
+echo -e "\nLoading docker image ..."
+docker load < $HOME/docker_ReID3D2025/large_files_docker_ReID3D2025/dockerimage_reid3d.tar.gz
+echo "Loaded!"
 
 # ターミナル出力
-echo "!!! Docker IMAGE kanazawa/reid3d_ready is ready !!!"
+echo -e "\n!!! Docker IMAGE kanazawa/reid3d_ready is ready !!!"
+echo "Please run <run-docker-container.sh>"
