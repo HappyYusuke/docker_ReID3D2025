16b5cde79613714ed82f788e4532afabe3ad0c78
Yusuke_Kanazawa <c1005073@planet.kanazawa-it.ac.jp>
2025-12-02 20:26:03 +0900

    リポジトリのクローン、依存関係のインストール、ビルドをするシェルスクリプトを追加

---
+++ home/setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# リポジトリのクローン
+mkdir -p ~/colcon_ws/src && cd ~/colcon_ws/src
+git clone https://github.com/GWxuan/ReID3D.git
+git clone https://github.com/Livox-SDK/livox_ros_driver2.git
+git clone https://github.com/Box-Robotics/ros2_numpy.git
+git clone https://github.com/HappyYusuke/harrp.git
+
+# 依存関係のパッケージをダウンロード
+cd ~/colcon_ws
+rosdep update
+rosdep install -i --from-path src --rosdistro foxy -y --ignore-src
+
+# ビルド
+cd ~/colcon_ws
+colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release
+source ~/colcon_ws/install/setup.bash
+
+# ホームディレクトリに戻る
+cd ~/
